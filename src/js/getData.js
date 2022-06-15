e38a3c8be835694a65acac6f01b63a26796d54a8
Waleed Amjad <waleedamjad99@hotmail.com>
2022-06-15 19:06:28 +0500

    Add GetData function

--- src/js/getData.js
+++ src/js/getData.js
@@ -1,15 +1,17 @@
 import displayItem from "./appFunctions";
-
+const container = document.getElementById('data-container');
 const getData = async () => {
     const response = await fetch(
-      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/'
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/6UXN3Bg7YgI82vpAte1w/scores/'
     );
     if (!response.ok) {
       throw new Error(`Something Went Wrong status: ${response.status}`);
     }
     const data = await response.json();
+    let item = '';
     data.result.forEach((score) => {
-      displayItem(score);
+        item += displayItem(score);
     });
-  };
+    container.innerHTML = item;
+};
 export default getData;
