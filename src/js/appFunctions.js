3fe4170e8ad6ac7575d1544f1c44541bda2d812e
Waleed Amjad <waleedamjad99@hotmail.com>
2022-06-15 19:07:10 +0500

    Add dist folder

--- src/js/appFunctions.js
+++ src/js/appFunctions.js
@@ -1,9 +1,33 @@
-const displayItem = (score) => {
-    const listPa = document.querySelector('.ulEl');
-    const listEl = document.createElement('li');
-    listEl.textContent = `${score.user}:${score.score}
-        `;
-    listPa.appendChild(listEl);
-  };
+import getResponse from './postData';
+import getData from './getData';
+// container.innerHTML = '';
+const refreshBtn = document.getElementById('refreshBtn');
+const Form = document.getElementById('Data-Form');
 
-  export default displayItem;
+const displayItem = (score) => {
+  return `<li>${score.user}:${score.score}</li>`;
+};
+
+Form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const userName = document.getElementById('username');
+  const score = document.getElementById('score');
+
+  if (score.value && userName.value) {
+    const scoreInfo = {
+      user: userName.value,
+      score: score.value,
+    };
+    userName.value = '';
+    score.value = '';
+    getResponse(scoreInfo);
+  }
+});
+
+
+refreshBtn.addEventListener('click', () => {
+    getData();
+  });
+  
+export default displayItem;
